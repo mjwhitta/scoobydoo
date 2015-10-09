6e9aacc7a4015514cd290fb639fa80ad2c811183
Miles Whittaker <mjwhitta@gmail.com>
2015-10-09 14:23:22 -0400

    Added caching

--- lib/scoobydoo.rb
+++ lib/scoobydoo.rb
@@ -1,7 +1,13 @@
 require "pathname"
 
 class ScoobyDoo
+    @@cache = Hash.new
+
     def self.where_are_you(cmd)
+        if (@@cache.has_key?(cmd))
+            return @@cache[cmd]
+        end
+
         if (cmd.include?(File::PATH_SEPARATOR))
             exe = Pathname.new(cmd).expand_path
             return (exe.executable? && !exe.directory?) ? exe : nil
@@ -12,6 +18,7 @@ class ScoobyDoo
             exts.each do |ext|
                 exe = File.join(path, "#{cmd}#{ext}")
                 if (File.executable?(exe) && !File.directory?(exe))
+                    @@cache[cmd] = exe
                     return exe
                 end
             end
--- scoobydoo.gemspec
+++ scoobydoo.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name = "scoobydoo"
-    s.version = "0.1.0"
+    s.version = "0.1.1"
     s.date = Time.new.strftime("%Y-%m-%d")
     s.summary = "Cross-platform which for ruby"
     s.description = "Cross-platform which for ruby."
