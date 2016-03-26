d964ae3b113b86cd026b84504ef0955836f1821a
Miles Whittaker <mjwhitta@gmail.com>
2016-03-26 19:17:09 -0400

    Fixed full path executables returning nil

--- lib/scoobydoo.rb
+++ lib/scoobydoo.rb
@@ -4,11 +4,9 @@ class ScoobyDoo
     @@cache = Hash.new
 
     def self.where_are_you(cmd)
-        if (@@cache.has_key?(cmd))
-            return @@cache[cmd]
-        end
+        return @@cache[cmd] if (@@cache.has_key?(cmd))
 
-        if (cmd.include?(File::PATH_SEPARATOR))
+        if (cmd.include?(File::SEPARATOR))
             exe = Pathname.new(cmd).expand_path
             return (exe.executable? && !exe.directory?) ? exe : nil
         end
@@ -23,6 +21,7 @@ class ScoobyDoo
                 end
             end
         end
+
         return nil
     end
 end
--- scoobydoo.gemspec
+++ scoobydoo.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name = "scoobydoo"
-    s.version = "0.1.3"
+    s.version = "0.1.4"
     s.date = Time.new.strftime("%Y-%m-%d")
     s.summary = "Cross-platform which for ruby"
     s.description = "Cross-platform which for ruby."
